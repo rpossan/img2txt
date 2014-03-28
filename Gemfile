fa137560e02ea1d3bee02dbb83436b6161cce553
ronaldo <ronaldo@ronaldo-fiberwork46.(none)>
2014-03-28 14:52:55 -0300

    Remove database configuration

--- Gemfile
+++ Gemfile
@@ -11,7 +11,9 @@ gem 'foundation-rails'
 gem 'rails', '4.0.4'
 
 # Use sqlite3 as the database for Active Record
-gem 'sqlite3'
+#gem 'sqlite3'
+
+gem 'pg'
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.2'
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,3 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-
-ActiveRecord::Schema.define(version: 20140325162051) do
-
-  create_table "posts", force: true do |t|
-    t.string   "post"
-    t.string   "title"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-end
